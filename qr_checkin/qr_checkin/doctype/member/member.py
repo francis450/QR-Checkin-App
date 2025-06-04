bb55d28992a24f724041ec2b130b6e520f13e92b
Njoroge Francis <franciskamande2001@gmail.com>
2025-06-04 07:57:43 +0300

    feat: Add Member DocType with validation for unique member IDs

---
+++ qr_checkin/qr_checkin/doctype/member/member.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2025, Njoroge Francis and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Member(Document):
+
+	def validate(self):
+		# check if member id is unique
+		if self.member_id:
+			existing_member = frappe.get_all(
+				"Member",
+				filters={"member_id": self.member_id, "name": ["!=", self.name]},
+				fields=["name"]
+			)
+			if existing_member:
+				frappe.throw(f"Member ID {self.member_id} already exists for another member.")
+
+	pass
