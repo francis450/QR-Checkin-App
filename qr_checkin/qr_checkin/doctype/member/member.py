3acec0dc54f236060bdfcf6469ec42dee69e0e24
= <franciskamande2001@gmail.com>
2025-06-09 16:01:02 +0300

    fix: Clean up import statement in Member DocType

--- qr_checkin/qr_checkin/doctype/member/member.py
+++ qr_checkin/qr_checkin/doctype/member/member.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2025, Njoroge Francis and contributors
-# For license information, please see license.txt
-
-# import frappe
+# For license information, please see license.tx
+import frappe
 from frappe.model.document import Document
 
 
@@ -10,6 +9,7 @@ class Member(Document):
 	def validate(self):
 		# check if member id is unique
 		if self.member_id:
+
 			existing_member = frappe.get_all(
 				"Member",
 				filters={"member_id": self.member_id, "name": ["!=", self.name]},
